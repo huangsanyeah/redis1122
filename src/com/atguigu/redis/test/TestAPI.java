8b7a7452e4cdbe88724efadb008d9dde69978212
huangweiyue <huangweiyue@quyiyuan.com>
2019-08-15 22:10:43 +0800

    shangGuiGu redis learn

---
+++ src/com/atguigu/redis/test/TestAPI.java
@@ -0,0 +1,25 @@
+package com.atguigu.redis.test;
+
+import redis.clients.jedis.Jedis;
+
+import java.util.Set;
+
+public class TestAPI {
+	public static void main(String[] args) 
+	{
+		Jedis jedis = new Jedis("127.0.0.1",6379);
+		jedis.auth("pass123");
+		
+		jedis.set("k1","v1");
+		jedis.set("k2","v2");
+		jedis.set("k3","v3");
+		
+		
+		System.out.println(jedis.get("k3"));
+		
+		Set<String> sets = jedis.keys("*");
+		System.out.println(sets.size());
+		
+		//后续请参考脑图，家庭作业，敲一遍......
+	}
+}
